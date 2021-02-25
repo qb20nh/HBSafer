43fe951298fa769b2a181e6f5f2064e718989de5
TJ <yut951121@gmail.com>
2021-02-25 15:01:23 +0900

    Do not log anything

--- src/background.js
+++ src/background.js
@@ -2,41 +2,27 @@ chrome.runtime.onInstalled.addListener(handleRuntimeInstalled);
 chrome.history.onVisited.addListener(handleHistoryVisited);
 
 function handleRuntimeInstalled(details) {
-	if (details.reason === 'install') {
-		chrome.storage.sync.set({
-			blacklist: [],
-			urlsPurged: 0,
-			startDate: Date.now()
-		}, () => {
-			console.log('Blacklist initialized.');
-		});
-	}
+  if (details.reason === "install") {
+    chrome.storage.sync.set({
+      blacklist: [],
+    });
+  }
 }
 
 function handleHistoryVisited(historyItem) {
-	chrome.storage.sync.get('blacklist', data => {
-		if (isBlacklisted(data.blacklist, historyItem.url)) {
-			console.log(`Blacklisted URL: ${historyItem.url}`);
-			purgeUrl(historyItem.url);
-		}
-	});
+  chrome.storage.sync.get("blacklist", (data) => {
+    if (isBlacklisted(data.blacklist, historyItem.url)) {
+      purgeUrl(historyItem.url);
+    }
+  });
 }
 
 function purgeUrl(url) {
-	chrome.history.deleteUrl({ url }, () => {
-		console.log(`History deleted for URL: ${url}`);
-		incrementCount();
-	});
-}
-
-function incrementCount() {
-	chrome.storage.sync.get('urlsPurged', data => {
-		chrome.storage.sync.set({
-			urlsPurged: data.urlsPurged + 1
-		});
-	});
+  chrome.history.deleteUrl({ url });
 }
 
 function isBlacklisted(blacklist, url) {
-	return blacklist.some(line => url.toLowerCase().includes(line.toLowerCase()));
+  return blacklist.some((line) =>
+    url.toLowerCase().includes(line.toLowerCase())
+  );
 }
