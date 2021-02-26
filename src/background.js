8060bc93f91969abb76ef0b85309ffee816fb1ec
TJ <yut951121@gmail.com>
2021-02-26 15:19:30 +0900

    Implement separate path and host matching

--- src/background.js
+++ src/background.js
@@ -1,10 +1,16 @@
+import("./lib/spark-md5.min.js");
+import("./lib/uri.all.min.js");
+
 chrome.runtime.onInstalled.addListener(handleRuntimeInstalled);
 chrome.history.onVisited.addListener(handleHistoryVisited);
 
 function handleRuntimeInstalled(details) {
   if (details.reason === "install") {
     chrome.storage.sync.set({
-      blacklist: [],
+      blacklist: [
+        "r8:2:1fd7de7da0fce4963f775a5fdb894db5:2:7ae7acb3afe3353f4e61b4746e02ddf6",
+        "f9:3:660328a7f9004d462085aa67a82065db:1:d41d8cd98f00b204e9800998ecf8427e",
+      ],
     });
   }
 }
@@ -21,8 +27,18 @@ function purgeUrl(url) {
   chrome.history.deleteUrl({ url });
 }
 
+/**
+ *
+ * @param {*} blacklist facebook.com reddit.com/r/funny www.youtube.com
+ * @param {*} url https://www.facebook.com http://reddit.com/
+ * 1. Separate blacklist item into domain and path parts
+ * 2. parse current url for domain and path
+ * 3. compare both url and path and exclude only if both match
+ */
 function isBlacklisted(blacklist, url) {
-  return blacklist.some((line) =>
-    url.toLowerCase().includes(line.toLowerCase())
-  );
+  return blacklist.some((line) => {
+    console.log("Hash of BL url:", SparkMD5.hash(line));
+    console.log("Hash of visited:", SparkMD5.hash(url));
+    return url.toLowerCase().includes(line.toLowerCase());
+  });
 }
--- src/options.js
+++ src/options.js
@@ -1,3 +1,43 @@
+/**
+ * Blacklist object data structure:
+ * [
+ *  {
+ *    hostname: '<hostHash>',
+ *    hostpart: <hostPartNum>,
+ *    pathname: '<pathHash>',
+ *    pathpart: <pathPartNum>
+ *  },
+ *  ...
+ * ]
+ * but to save storage space, we will save all values into ordered array:
+ * [
+ *  hostname, hostpart, pathname, pathpart,
+ *  hostname, hostpart, pathname, pathpart,
+ *  hostname, hostpart, pathname, pathpart,
+ *  ...
+ * ]
+ */
+
+function digestBlacklistURL(url) {
+  var uri = URI.parse(url);
+  var hostname = uri.host;
+  if (!hostname) {
+    uri = URI.parse("http://" + url);
+    hostname = uri.host;
+  }
+  var hostpart = hostname.split(".").length;
+  var pathname = uri.path;
+  if (pathname.startsWith("/")) pathname = pathname.slice(1);
+  if (pathname.endsWith("/")) pathname = pathname.slice(0, pathname.length - 1);
+  var pathpart = pathname.split("/").length;
+  var domain = psl.parse(hostname).domain || hostname;
+  var hosthash = SparkMD5.hash(hostname);
+  var pathhash = SparkMD5.hash(pathname);
+  return `${domain[0]}${
+    domain.length - 1
+  }:${hostpart}:${hosthash}:${pathpart}:${pathhash}`;
+}
+
 chrome.storage.sync.get("blacklist", (data) => {
   if (!data.blacklist) {
     throw new Error("expected blacklist in storage");
@@ -5,26 +45,6 @@ chrome.storage.sync.get("blacklist", (data) => {
 
   const blacklistEl = document.getElementById("blacklist");
   blacklistEl.value = data.blacklist.join("\n");
-});
-
-chrome.storage.sync.get(["urlsPurged", "startDate"], (data) => {
-  if (data.urlsPurged == null) {
-    throw new Error("expected urlsPurged in storage");
-  }
-  if (data.startDate == null) {
-    throw new Error("expected startDate in storage");
-  }
-
-  document.getElementById(
-    "urls-purged"
-  ).innerHTML = data.urlsPurged.toLocaleString();
-  document.getElementById("urls-purged-date").innerHTML = new Date(
-    data.startDate
-  ).toLocaleDateString();
-
-  if (data.urlsPurged > 0) {
-    document.getElementById("urls-purged-container").className = "show";
-  }
 });
 
 const saveEl = document.getElementById("save");
